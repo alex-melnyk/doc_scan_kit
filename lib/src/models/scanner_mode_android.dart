5213302f1fd2e976465a9029e02b23868a8145d3
Alex Melnyk <ask.melnyk@gmail.com>
2025-10-16 13:41:42 +0300

    Add docs

--- lib/src/models/scanner_mode_android.dart
+++ lib/src/models/scanner_mode_android.dart
@@ -1,11 +1,16 @@
 /// ScannerModeAndroid is an enum that represents the scanner mode on Android.
 enum ScannerModeAndroid {
-  /// Base mode.
+  /// Basic editing capabilities (crop, rotate, reorder pages, etc…).
   base,
 
-  /// Filter mode.
+  /// Adds image filters (grayscale, auto image enhancement, etc…)
+  /// to the [base] mode.
   filter,
 
-  /// Full mode.
+  /// Adds ML-enabled image cleaning capabilities (erase stains, fingers, etc…)
+  /// to the [filter] mode. This mode will also allow future major features
+  /// to be automatically added along with Google Play services updates,
+  /// while the other two modes will maintain their current feature sets
+  /// and only receive minor refinements.
   full;
 }
